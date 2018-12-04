b01e32398771d51d5f8acbb373ea1bb145e90601
phannguyentri <phnguyentri@gmail.com>
2018-12-05 01:46:07 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state  = {
+      txtUsername   : '',
+      txtPassword   : '',
+      gender        : 1,
+      lang          : "en",
+      cbEnable      : true
+    };
+    this.onHandleChange = this.onHandleChange.bind(this);
+    this.onHandleSubmit = this.onHandleSubmit.bind(this);
+  }
+
+  onHandleChange(e){
+    let value = (e.target.type === 'checkbox') ? e.target.checked : e.target.value;
+    this.setState({
+      [e.target.name] : value
+    });
+  }
+
+  onHandleSubmit(e){
+    console.log(this.state);
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <h2>Form control: input</h2>
+          <h3> { this.state.txtUsername }, { this.state.txtPassword }, { (parseInt(this.state.gender) === 0) ? 'Male' : 'Female' } </h3>
+          <form onSubmit={this.onHandleSubmit}>
+            <div className="form-group">
+              <label htmlFor="usr">Name:</label>
+              <input type="text" name="txtUsername" className="form-control" onChange={ this.onHandleChange }/>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="pwd">Password:</label>
+              <input type="password" name="txtPassword" className="form-control" onChange={ this.onHandleChange }/>
+            </div>
+
+            <div className="form-group">
+              <select name="gender" className="form-control" value={this.state.gender} onChange={ this.onHandleChange }>
+                <option value={0}>Male</option>
+                <option value={1}>Female</option>
+              </select>
+            </div>
+
+            <div className="form-group">
+              <div className="radio">
+                <label><input type="radio" name="lang" value="en" checked={this.state.lang === "en"} onChange={ this.onHandleChange } />English</label>
+              </div>
+              <div className="radio">
+                <label><input type="radio" name="lang" value="vi" checked={this.state.lang === "vi"} onChange={ this.onHandleChange } />Vietnames</label>
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label className="checkbox">
+                <input
+                  type="checkbox"
+                  name="cbEnable"
+                  value={true}
+                  onChange={ this.onHandleChange }
+                  checked={this.state.cbEnable === true}
+                /> Enable
+              </label>
+            </div>
+
+
+            <button type="submit" className="btn btn-success  ">Save</button>
+          </form>
+        </div>
       </div>
     );
   }
